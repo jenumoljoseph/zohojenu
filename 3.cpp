de8fb528583f7d36933036d815a24439fd48395d
JENUMOL JOSEPH <jenumoljoseph@gmail.com>
2022-08-21 23:26:45 +0530

    3

---
+++ 3.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int sort(int a);
+  int a[]={-1,150,190,170,-1,-1,160,180};
+  a=sort(a);
+  for(int i=0;i<len(a);i++)
+  {
+    cout<<a[i];
+  }
+
+}
+
+int sort(int a)
+{
+  if(i!=-1)
+   b=i;
+  for(i=0;i<len(a);i++)
+  {
+    if(a[i]==-1)
+     continue;
+    else
+    {
+      a[i]=b[j];
+      j++
+    }
+  }
+  return a;
+}
